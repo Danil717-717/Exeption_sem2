eb79f918b4479be25ec7d4e0965507f2d24081fe
Данил <danila_monster@mail.ru>
2023-04-12 23:48:09 +0300

    HW_02-03

---
+++ src/HW_02.java
@@ -0,0 +1,18 @@
+//Если необходимо, исправьте данный код
+
+public class HW_02 {
+
+    public static void divideNumArr(){
+        try {
+            int[] intArray = {1,2,3,4,5,6,7,8,9};
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+
+
+
+}
---
+++ src/HW_03.java
@@ -0,0 +1,23 @@
+//Дан следующий код, исправьте его там, где требуется
+
+
+import java.io.FileNotFoundException;
+
+public class HW_03 {
+    public static void main(String[] args) throws Exception {
+        try {
+            int a = 90;
+            int b = 3;
+            System.out.println(a / b);
+            printSum(23, 234);
+            int[] abc = { 1, 2 };
+            abc[3] = 9;
+        } catch (IndexOutOfBoundsException ex) {
+            System.out.println("Массив выходит за пределы своего размера!");
+        }
+    }
+    public static void printSum(Integer a, Integer b) throws FileNotFoundException {
+        System.out.println(a + b);
+    }
+
+}
